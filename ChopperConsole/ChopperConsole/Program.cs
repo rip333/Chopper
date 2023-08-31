5415ea39cc7f63b03ed47f6cb3d139b7fb81b021
Rip Britton <ripbritton3@gmail.com>
2023-08-31 16:39:18 -0500

    deleting stuff

--- ChopperConsole/ChopperConsole/Program.cs
+++ ChopperConsole/ChopperConsole/Program.cs
@@ -11,42 +11,10 @@ namespace ChopperConsole
             
             Console.WriteLine("Chopper!");
 
-            var ninja = Hero.FromJson(FileReader.ReadUrl("https://raw.githubusercontent.com/rip333/Chopper/main/playable-heroes/Ninja.json"));
-            var knight =
-                Hero.FromJson(
-                    FileReader.ReadUrl(
-                        "https://raw.githubusercontent.com/rip333/Chopper/main/playable-heroes/Knight.json"));
-            
-            Console.WriteLine("D for draw.  S for shuffle.  Q for quit.");
-            Console.WriteLine("");
-            Console.WriteLine("Shuffling..");
-            ninja.Shuffle();
-            knight.Shuffle();
-            
-            var playing = true;
 
             while (playing)
             {
-                var input = Console.ReadKey();
-                Console.WriteLine("");
-                switch (input.KeyChar)
-                {
-                    case 'q':
-                        playing = false;
-                        break;
-                    case 'd':
-                        Console.WriteLine("Ninja: " + ninja.DrawCard());
-                        Console.WriteLine("Ninja: " + ninja.DrawCard());
-                        Console.WriteLine("...");
-                        Console.WriteLine("Knight: " + knight.DrawCard());
-                        Console.WriteLine("Knight: " + knight.DrawCard());
-                        break;
-                    case 's':
-                        Console.WriteLine("Shuffling..");
-                        ninja.Shuffle();
-                        knight.Shuffle();
-                        break;
-                }
+
             }
 
         }
--- chopper/models/game/ability.ts
+++
@@ -1,7 +0,0 @@
-export class Ability {
-    public name: string = "";
-    public description: string = "";
-    public isUltimate: boolean = false;
-    public imageUrl: string = "";
-    public rarity: string = "common";
-}
